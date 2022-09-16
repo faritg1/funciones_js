60fb8cf9685f0a9b6ac3b675cdc6b4a844659e7c
carlos torres <juanagelves@gmail.com>
2022-09-16 15:29:47 -0500

    tabla de multiplicacion

--- main.js
+++ main.js
@@ -8,11 +8,9 @@ const number=[30,40,50,60]
 for (let atri in person){
     console.log(person[atri])
 }
-
 for(let ele of number){
     console.log(ele)
 } */
-
 /* declarada */
 /* function mostrar(){
 
@@ -21,9 +19,16 @@ for(let ele of number){
 /* const muestra = function (){
 
 }  */
+/*
 
 function sumar(a,b) {
     let suma = a+b
     console.log(suma)
 }
-sumar(2,3)
+sumar(2,3) */
+
+let number =+ prompt("Give me a number")
+
+for (let i = 1; i <=9; i++) {
+    document.write(`${number}x${i} = ${number*i} <br>`)
+}
