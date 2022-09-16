1ac0bf380de635af3a758b5b149e3532a58609bb
carlos torres <juanagelves@gmail.com>
2022-09-16 14:25:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const person ={
+    name: "pepe",
+    lastname: "perez",
+    age: 30,
+    country: "colombia"
+}
+const number=[30,40,50,60]
+for (let atri in person){
+    console.log(person[atri])
+}
+
+for(let ele of number){
+    console.log(ele)
+}
